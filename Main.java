55389c09f86e5faaf428da3d2ff333c82c9657a0
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-16 21:19:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+//Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, k, j;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz : ");
+        k = input.nextInt();
+        System.out.println("4'ün kuvvetleri :");
+
+        for (i = 1; i <= k; i *= 4) {
+            System.out.println(i);
+        }
+        System.out.println("************");
+        System.out.println("5'in kuvvetleri :");
+        for (j = 1; j <= k; j *= 5) {
+            System.out.println(j);
+        }
+    }
+}
